774dec9fabf2eef4c648283a5c12072f01e23e7a
Phil1234567 <pgrenon.pg@gmail.com>
2018-07-04 14:24:08 -0400

    Beat indicator as a series of circles

--- Snake/Assets/Scripts/BeatIndicator.cs
+++ Snake/Assets/Scripts/BeatIndicator.cs
@@ -3,21 +3,24 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using DG.Tweening;
+using System;
 
 public class BeatIndicator : MonoBehaviour {
 
     public Bar Bar;
     public AudioClip LowBeat;
     public AudioClip HighBeat;
-    public Sprite Quarter;
-    public Sprite Third;
-    public Sprite Half;
+    public GameObject BeatLightPrefab;
     public Color STMYellow;
     public Color STMBlue;
+    //public Sprite Quarter;
+    //public Sprite Third;
+    //public Sprite Half;
 
-    private List<Image> _beatSections;
+    private List<BeatLight> _beatLights;
     private List<AudioSource> _beatSources;
-    private Sprite _currentSection;
+    //private Sprite _currentSection;
+    //private List<Image> _beatSections;
 
     // Use this for initialization
     void Start () {
@@ -37,11 +40,11 @@ public class BeatIndicator : MonoBehaviour {
     {
         Bar = newBar;
 
-        if (_beatSections != null)
+        if (_beatLights != null)
         {
-            for (int i = 0; i < _beatSections.Count; i++)
+            for (int i = 0; i < _beatLights.Count; i++)
             {
-                Destroy(_beatSections[i].gameObject);
+                Destroy(_beatLights[i].gameObject);
             }
         }
 
@@ -50,36 +53,33 @@ public class BeatIndicator : MonoBehaviour {
 
     public void CreateIndicator()
     {
-        switch (Bar.Beats.Count)
-        {
-            case 2:
-                _currentSection = Half;
-                break;
-            case 3:
-                _currentSection = Third;
-                break;
-            default:
-                _currentSection = Quarter;
-                break;
-        }
+        //switch (Bar.Beats.Count)
+        //{
+        //    case 2:
+        //        _currentSection = Half;
+        //        break;
+        //    case 3:
+        //        _currentSection = Third;
+        //        break;
+        //    default:
+        //        _currentSection = Quarter;
+        //        break;
+        //}
 
-        _beatSections = new List<Image>();
+        _beatLights = new List<BeatLight>();
 
-        for (int i = 0; i < Bar.Beats.Count; i++)
+        for (int i = 0; i < Bar.Beats.Count - 1; i++)
         {
-            Image beatSection = new GameObject("Beat " + i).AddComponent<Image>();
-            beatSection.transform.SetParent(transform, false);
-            beatSection.sprite = _currentSection;
-            beatSection.transform.Rotate(new Vector3(0, 0, -360/ Bar.Beats.Count * i));
-            if (i == Bar.Beats.Count - 1)
-            {
-                beatSection.color = STMBlue;
-            }
-            else
-            {
-                beatSection.color = STMYellow;
-            }
-            _beatSections.Add(beatSection);
+            var beatLight = Instantiate(BeatLightPrefab, transform).GetComponent<BeatLight>();
+
+            //Image beatSection = new GameObject("Beat " + i).AddComponent<Image>();
+            //beatSection.transform.SetParent(transform, false);
+            //beatSection.sprite = _currentSection;
+            //beatSection.transform.Rotate(new Vector3(0, 0, -360/ Bar.Beats.Count * i));
+
+            beatLight.Light.color = STMYellow;
+
+            _beatLights.Add(beatLight);
         }
     }
 
@@ -92,12 +92,20 @@ public class BeatIndicator : MonoBehaviour {
     {
         for (var i = 0; i < Bar.Beats.Count; i++)
         {
+
             yield return new WaitForSeconds(Bar.Beats[i].Delay);
 
             if (i == 0)
             {
-                ResetSection(_beatSections[_beatSections.Count - 1]);
+                for (int ii = 0; ii < _beatLights.Count; ii++)
+                {
+                    ResetLight(_beatLights[ii]);
+                }
             }
+            //if (i == 0)
+            //{
+            //    ResetSection(_beatSections[_beatSections.Count - 1]);
+            //}
 
             if (!Bar.Beats[i].IsHigh)
             {
@@ -115,24 +123,42 @@ public class BeatIndicator : MonoBehaviour {
                 }
             }
 
-            _beatSections[i].transform.DOScale(Vector3.one * 1.5f, Bar.Beats[i].Delay);
-            _beatSections[i].DOFade(0, Bar.Beats[i].Delay);
+            if (i == Bar.Beats.Count - 1)
+            {
+                for (int ii = 0; ii < _beatLights.Count; ii++)
+                {
+                    _beatLights[ii].Light.color = STMBlue;
+                    _beatLights[ii].transform.DOPunchScale(Vector3.one / 5, Bar.Beats[i].Delay / 5,1);
+                }
+            }
+            else
+            {
+                _beatLights[i].Light.DOFade(1, .1f);
+            }
+
+            //_beatSections[i].transform.DOScale(Vector3.one * 1.5f, Bar.Beats[i].Delay);
+            //_beatSections[i].DOFade(0, Bar.Beats[i].Delay);
         }
 
         StartCoroutine(Beat());
 
-        for (int i = 0; i < _beatSections.Count - 1; i++)
-        {
-            ResetSection(_beatSections[i]);
-        }
+        //for (int i = 0; i < _beatSections.Count - 1; i++)
+        //{
+        //    ResetSection(_beatSections[i]);
+        //}
     }
 
-    private void ResetSection(Image section)
+    private void ResetLight(BeatLight beatLight)
     {
-        section.transform.localScale = Vector3.zero;
-        var tempColor = section.color;
-        tempColor.a = 1f;
-        section.color = tempColor;
-        section.transform.DOScale(Vector3.one, 0.1f);
+        beatLight.Light.DOColor(STMYellow, .1f);
     }
+
+    //private void ResetSection(Image section)
+    //{
+    //    section.transform.localScale = Vector3.zero;
+    //    var tempColor = section.color;
+    //    tempColor.a = 1f;
+    //    section.color = tempColor;
+    //    section.transform.DOScale(Vector3.one, 0.1f);
+    //}
 }
